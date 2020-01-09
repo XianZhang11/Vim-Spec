7c875ebed0d933e8951dfd8f5340323eedce8398
Xian Zhang <xian@Nevermore>
2020-01-09 21:25:36 +0000

    add notes

--- s.js
+++ s.js
@@ -7,44 +7,44 @@ const hello = 'hello world!';
 
 
 app.get('//video', function(req, res) {
-	    console.log('video');
-	    const path = 'sample.mp4'
-	    const stat = fs.statSync(path)
-	    const fileSize = stat.size
-	    const range = req.headers.range
-	    if (range) {
-		          const parts = range.replace(/bytes=/, "").split("-")
-		          const start = parseInt(parts[0], 10)
-		          const end = parts[1] 
-		            ? parseInt(parts[1], 10)
-		            : fileSize-1
-		          const chunksize = (end-start)+1
-		          const file = fs.createReadStream(path, {start, end})
-		          const head = {
-				          'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-				          'Accept-Ranges': 'bytes',
-				          'Content-Length': chunksize,
-				          'Content-Type': 'video/mp4',
-				        }
-		          res.writeHead(206, head);
-		          file.pipe(res);
-		        } else {
-				      const head = {
-					              'Content-Length': fileSize,
-					              'Content-Type': 'video/mp4',
-					            }
-				      res.writeHead(200, head)
-				      fs.createReadStream(path).pipe(res)
-				    }
-	  });
+	console.log('video');
+	const path = 'sample.mp4'
+	const stat = fs.statSync(path)
+	const fileSize = stat.size
+	const range = req.headers.range
+	if (range) {
+		const parts = range.replace(/bytes=/, "").split("-")
+		const start = parseInt(parts[0], 10)
+		const end = parts[1] 
+			? parseInt(parts[1], 10)
+			: fileSize-1
+		const chunksize = (end-start)+1
+		const file = fs.createReadStream(path, {start, end})
+		const head = {
+			'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+			'Accept-Ranges': 'bytes',
+			'Content-Length': chunksize,
+			'Content-Type': 'video/mp4',
+		}
+		res.writeHead(206, head);
+		file.pipe(res);
+	} else {
+		const head = {
+			'Content-Length': fileSize,
+			'Content-Type': 'video/mp4',
+		}
+		res.writeHead(200, head)
+		fs.createReadStream(path).pipe(res)
+	}
+});
 
 const w = fs.readFileSync('input','utf-8');
 
 app.get('/',(req,res) => {
-	   res.end(w);
+	res.end(w);
 });
 
 app.listen(10086,'localhost', ()=>{
-	    console.log('server listening!');
+	console.log('server listening!');
 });
 what is thsi?
---
+++ somenotes.md
@@ -0,0 +1,9 @@
+# Some useful commands
+
+-:noh	:shortcut for :nohlsearch
+
+gg=G	fix indentation
+
+ctrl+o	last cursor position
+ctrl+i	next cursor position
+
